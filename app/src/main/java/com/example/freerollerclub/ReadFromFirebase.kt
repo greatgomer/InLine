9003381a443c9ecd51ef1b5ade120cedb63775ba
Victor Spirin <greatgomer2@gmail.com>
2020-06-04 19:17:38 +0300

    Add separation in firebase

--- app/src/main/java/com/example/freerollerclub/ReadFromFirebase.kt
+++ app/src/main/java/com/example/freerollerclub/ReadFromFirebase.kt
@@ -10,32 +10,32 @@ import com.google.firebase.database.*
 class ReadFromFirebase {
     private lateinit var database: DatabaseReference
     private var places_name = "Places"
-    val city = ArrayList<String>(1)
+    var cities: MutableList<String> = mutableListOf<String>()
 
-    fun getDataFromDatabase(mMap: GoogleMap) {
-        city.add("Rostov")
-        city.add("Moscow")
-        for (i in city) {
-            database = FirebaseDatabase.getInstance().getReference(places_name).child(i)
-            val valueEventListener = object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    for (ds in dataSnapshot.children) {
-                        val lat = ds.child("lat").getValue(String::class.java)
-                        val lon = ds.child("lon").getValue(String::class.java)
-                        val name = ds.child("name").getValue(String::class.java)
-                        val options = ds.child("options").getValue(String::class.java)
+    fun getDataFromDatabase(mMap: GoogleMap){
+        database = FirebaseDatabase.getInstance().getReference(places_name)
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (ds in dataSnapshot.children) {
+                    val city = ds.key
+                    cities.add(city.toString())
+                    for (childSnapshot in ds.children) {
+                        val lat = childSnapshot.child("lat").getValue(String::class.java)
+                        val lon = childSnapshot.child("lon").getValue(String::class.java)
+                        val name = childSnapshot.child("name").getValue(String::class.java)
+                        val options = childSnapshot.child("options").getValue(String::class.java)
 
                         val marker = LatLng(lat!!.toDouble(), lon!!.toDouble())
                         mMap.addMarker(MarkerOptions().position(marker).title(name).snippet(options))
 
                     }
                 }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    Log.d(TAG, databaseError.getMessage()) //Don't ignore errors!
-                }
             }
-            database.addListenerForSingleValueEvent(valueEventListener)
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, databaseError.getMessage()) //Don't ignore errors!
+            }
         }
+        database.addListenerForSingleValueEvent(valueEventListener)
     }
 }
