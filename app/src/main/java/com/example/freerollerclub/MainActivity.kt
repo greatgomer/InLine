3654c9ff7e743e98b894571b932c3854deddb53d
Victor Spirin <greatgomer2@gmail.com>
2020-06-08 12:08:16 +0300

    Ofline is ready

--- app/src/main/java/com/example/freerollerclub/MainActivity.kt
+++ app/src/main/java/com/example/freerollerclub/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
     }
 
     fun onMap(view: View) {
