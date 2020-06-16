d8f617aaf211172bdd954368f42851d2e3a9fbad
Victor Spirin <greatgomer2@gmail.com>
2020-06-16 13:48:31 +0300

    Markers split on shops and points

--- app/src/main/java/com/svdgroup/inline/MarkersLocation.kt
+++ app/src/main/java/com/svdgroup/inline/MarkersLocation.kt
@@ -12,18 +12,20 @@ class MarkersLocation {
     private lateinit var database: DatabaseReference
     private val citiesTitle = "Cities"
     private val markersTitle = "Markers"
+    private var markerContent = ""
 
     fun getDataFromDatabase(mMap: GoogleMap){
         database = AppDatabase.getDatabase()!!.getReference(citiesTitle)
         val valueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (ds in dataSnapshot.children) {
-                    for (childSnapshot in ds.child(markersTitle).child("Points").children) {
-                        markers(mMap, childSnapshot)
+                    markerContent = "Points"
+                    for (childSnapshot in ds.child(markersTitle).child(markerContent).children) {
+                        markers(mMap, childSnapshot, markerContent)
                     }
-
-                    for (childSnapshot in ds.child(markersTitle).child("Shops").children) {
-                        markers(mMap, childSnapshot)
+                    markerContent = "Shops"
+                    for (childSnapshot in ds.child(markersTitle).child(markerContent).children) {
+                        markers(mMap, childSnapshot, markerContent)
                     }
                 }
             }
@@ -36,13 +38,21 @@ class MarkersLocation {
     }
 }
 
-fun markers(mMap: GoogleMap, childSnapshot: DataSnapshot){
+fun markers(mMap: GoogleMap, childSnapshot: DataSnapshot, markerContent: String){
     val lat = childSnapshot.child("lat").getValue(String::class.java)
     val lon = childSnapshot.child("lon").getValue(String::class.java)
     val name = childSnapshot.child("name").getValue(String::class.java)
     val options = childSnapshot.child("options").getValue(String::class.java)
 
     val marker = LatLng(lat!!.toDouble(), lon!!.toDouble())
-    mMap.addMarker(MarkerOptions().position(marker).title(name).snippet(options).icon(
-        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)))
+
+    if(markerContent == "Points") {
+        mMap.addMarker(MarkerOptions().position(marker).title(name).snippet(options))
+    }else{
+        mMap.addMarker(
+            MarkerOptions().position(marker).title(name).snippet(options).icon(
+                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)
+            )
+        )
+    }
 }
