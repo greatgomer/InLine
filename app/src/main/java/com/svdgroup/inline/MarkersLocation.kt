52e3ec60609c0c9e258977fc0b33fd932b20e3cc
Victor Spirin <greatgomer2@gmail.com>
2020-06-16 13:23:01 +0300

    Markers split

--- app/src/main/java/com/svdgroup/inline/MarkersLocation.kt
+++ app/src/main/java/com/svdgroup/inline/MarkersLocation.kt
@@ -3,6 +3,7 @@ package com.svdgroup.inline
 import android.content.ContentValues.TAG
 import android.util.Log
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.*
@@ -17,14 +18,12 @@ class MarkersLocation {
         val valueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (ds in dataSnapshot.children) {
-                    for (childSnapshot in ds.child(markersTitle).children) {
-                        val lat = childSnapshot.child("lat").getValue(String::class.java)
-                        val lon = childSnapshot.child("lon").getValue(String::class.java)
-                        val name = childSnapshot.child("name").getValue(String::class.java)
-                        val options = childSnapshot.child("options").getValue(String::class.java)
+                    for (childSnapshot in ds.child(markersTitle).child("Points").children) {
+                        markers(mMap, childSnapshot)
+                    }
 
-                        val marker = LatLng(lat!!.toDouble(), lon!!.toDouble())
-                        mMap.addMarker(MarkerOptions().position(marker).title(name).snippet(options))
+                    for (childSnapshot in ds.child(markersTitle).child("Shops").children) {
+                        markers(mMap, childSnapshot)
                     }
                 }
             }
@@ -35,4 +34,15 @@ class MarkersLocation {
         }
         database.addListenerForSingleValueEvent(valueEventListener)
     }
+}
+
+fun markers(mMap: GoogleMap, childSnapshot: DataSnapshot){
+    val lat = childSnapshot.child("lat").getValue(String::class.java)
+    val lon = childSnapshot.child("lon").getValue(String::class.java)
+    val name = childSnapshot.child("name").getValue(String::class.java)
+    val options = childSnapshot.child("options").getValue(String::class.java)
+
+    val marker = LatLng(lat!!.toDouble(), lon!!.toDouble())
+    mMap.addMarker(MarkerOptions().position(marker).title(name).snippet(options).icon(
+        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)))
 }
